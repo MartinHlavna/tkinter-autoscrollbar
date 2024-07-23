b627c868711818adf28b72d09c45748e5028c6d5
Martin Hlavňa <martin.hlavna@4q.sk>
2024-07-23 14:32:44 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tkinter-autoscrollbar',
+    version='1.0.0',
+    url='https://github.com/MartinHlavna/tkinter-autoscrollbar',
+    author='Martin Hlavňa',
+    author_email='info@martinhlavna.sk',
+    description='Scrollbar for tkinter that hides itself when not needed',
+    packages=['tkinter_autoscrollbar'],
+    install_requires=[],
+)
